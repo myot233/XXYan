d32569ddb14034b8c115d5aff61cf563dea39782
gsycl2004 <1544189298@qq.com>
2022-05-17 09:51:58 +0800

    给config增加了提示

--- src/main/kotlin/YanConfig.kt
+++ src/main/kotlin/YanConfig.kt
@@ -1,9 +1,12 @@
 package com.github
 
 import net.mamoe.mirai.console.data.AutoSavePluginConfig
+import net.mamoe.mirai.console.data.ValueDescription
 import net.mamoe.mirai.console.data.value
 
 object YanConfig:AutoSavePluginConfig("config") {
+    @ValueDescription("一个用于指定指令对应触发的列表")
     val cares:MutableMap<String,Long> by value(mutableMapOf("1" to 1L))
+    @ValueDescription("这里可以指定字体")
     val font by value("sarasa-ui-tc-regular.ttf")
 }
